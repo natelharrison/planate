380d627046821b8808adb1c3264a8ac22640964c
planit <planit@tanjeffreyz.github.io>
2024-10-05 22:34:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -32,8 +32,8 @@ assignments = {
       "title": "Lab 04",
       "course": "CS 198-008",
       "dueDate": "2024-10-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/844937/assignments/4833363/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/844937/assignments/4833363/submissions/274915729",
+      "submitted": true
     },
     {
       "title": "Vitamin 2",
