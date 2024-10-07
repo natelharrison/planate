34dfc6d509da9da3572de364f20f13a894e66791
planit <planit@tanjeffreyz.github.io>
2024-10-07 17:34:01 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -227,6 +227,20 @@ assignments = {
       "dueDate": "2024-10-06T23:59:00",
       "link": "https://www.gradescope.com/courses/845267/assignments/5083124/submissions/275171506",
       "submitted": true
+    },
+    {
+      "title": "Lab 5.5 Code",
+      "course": "Data C102",
+      "dueDate": "2024-10-11T17:00:00",
+      "link": "https://www.gradescope.com/courses/845267",
+      "submitted": false
+    },
+    {
+      "title": "Lab 5.5 Written",
+      "course": "Data C102",
+      "dueDate": "2024-10-11T17:00:00",
+      "link": "https://www.gradescope.com/courses/845267",
+      "submitted": false
     }
   ],
   "Physics 188/288": [
