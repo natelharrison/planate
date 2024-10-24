7709e1b54c35d2e96a55d9a767d730c065c770a4
planit <planit@tanjeffreyz.github.io>
2024-10-24 06:38:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -124,7 +124,7 @@ assignments = {
     {
       "title": "HW2",
       "course": "DATA C182-LEC-001",
-      "dueDate": "2024-10-27T23:59:00",
+      "dueDate": "2024-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/837491",
       "submitted": false
     },
