6f098a346969f6dc3cf5c890694b8ec7945bff1a
planit <planit@tanjeffreyz.github.io>
2024-10-24 00:47:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -148,6 +148,13 @@ assignments = {
       "dueDate": "2024-10-27T23:59:00",
       "link": "https://www.gradescope.com/courses/837491/assignments/5192483/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "HW2 (Written PDF)",
+      "course": "DATA C182-LEC-001",
+      "dueDate": "2024-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/837491",
+      "submitted": false
     }
   ],
   "Data C102": [
