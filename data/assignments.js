8d6af5b12c3592400d7b9583ad9727710c3ef03a
planit <planit@tanjeffreyz.github.io>
2024-10-25 00:47:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -344,7 +344,7 @@ assignments = {
       "title": "Vitamin 8",
       "course": "Data C102",
       "dueDate": "2024-10-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/845267",
+      "link": "https://www.gradescope.com/courses/845267/assignments/5201740/submissions/new",
       "submitted": false
     },
     {
