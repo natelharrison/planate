a01529408706e6956bb5211a8d194e0fccb2853c
planit <planit@tanjeffreyz.github.io>
2024-10-28 21:32:40 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -169,6 +169,13 @@ assignments = {
       "dueDate": "2024-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/837491",
       "submitted": false
+    },
+    {
+      "title": "Course Survey (Week 10) (optional, extra credit)",
+      "course": "DATA C182-LEC-001",
+      "dueDate": "2024-11-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/837491/assignments/5233589/submissions/new",
+      "submitted": false
     }
   ],
   "Data C102": [
