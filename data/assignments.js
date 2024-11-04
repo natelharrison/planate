9e97dbe6b4ca453242b4ed425cb74bff424b2722
planit <planit@tanjeffreyz.github.io>
2024-11-04 21:32:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -444,6 +444,13 @@ assignments = {
       "dueDate": "2024-11-06T17:00:00",
       "link": "https://www.gradescope.com/courses/845267",
       "submitted": false
+    },
+    {
+      "title": "Project Proposal",
+      "course": "Data C102",
+      "dueDate": "2024-11-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/845267",
+      "submitted": false
     }
   ],
   "Physics 188/288": [
