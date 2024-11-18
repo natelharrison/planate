c9330281b596630d86ca97e2a17045868e90bf61
planit <planit@tanjeffreyz.github.io>
2024-11-18 04:37:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -477,8 +477,8 @@ assignments = {
       "title": "Vitamin 11",
       "course": "Data C102",
       "dueDate": "2024-11-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/845267/assignments/5326743/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/845267/assignments/5326743/submissions/290107362",
+      "submitted": true
     },
     {
       "title": "Lab 10 Written",
