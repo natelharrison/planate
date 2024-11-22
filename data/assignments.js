b68389dd2433352da8001abc21635d0fe7e22ba1
planit <planit@tanjeffreyz.github.io>
2024-11-22 22:36:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -518,7 +518,7 @@ assignments = {
     {
       "title": "Homework 5",
       "course": "Data C102",
-      "dueDate": "2024-11-22T17:00:00",
+      "dueDate": "2024-11-24T17:00:00",
       "link": "https://www.gradescope.com/courses/845267",
       "submitted": false
     },
