b52a088b3f8118437dacec46f91ae27ba9674849
planit <planit@tanjeffreyz.github.io>
2024-11-27 23:33:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -223,8 +223,8 @@ assignments = {
       "title": "HW3",
       "course": "DATA C182-LEC-001",
       "dueDate": "2024-11-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/837491",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/837491/assignments/5234217/submissions/293295961",
+      "submitted": true
     },
     {
       "title": "Course Survey (Week 11) (optional, extra credit)",
